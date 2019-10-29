775fabfc06795660efaddb57c223a28fb4487653
SgtBagus <procw57@gmail.com>
2019-10-29 21:23:07 +0700

    asd

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -10,7 +10,6 @@
   <link rel="stylesheet" href="<?= base_url('assets/') ?>bower_components/Ionicons/css/ionicons.min.css">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>dist/css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/') ?>bower_components/morris.js/morris.css">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>bower_components/jvectormap/jquery-jvectormap.css">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>owl/docs/assets/owlcarousel/assets/owl.carousel.min.css">
@@ -18,6 +17,7 @@
   <link rel="stylesheet" href="<?= base_url('assets/') ?>plugins/iCheck/all.css">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>bower_components/select2/dist/css/select2.min.css">
 
+  <link rel="icon" href="<?= base_url('assets/') ?>img/logo.jpg">
   <link rel="stylesheet" href="<?= base_url('assets/') ?>custom_css.css">
   <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 
@@ -88,7 +88,6 @@ if ($this->session->userdata('session_sop') == "") {
   </script>
   <script src="<?= base_url('assets/') ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   <script src="<?= base_url('assets/') ?>bower_components/raphael/raphael.min.js"></script>
-  <script src="<?= base_url('assets/') ?>bower_components/morris.js/morris.min.js"></script>
   <script src="<?= base_url('assets/') ?>bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
   <script src="<?= base_url('assets/') ?>plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
   <script src="<?= base_url('assets/') ?>plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
@@ -102,7 +101,6 @@ if ($this->session->userdata('session_sop') == "") {
   <script src="<?= base_url('assets/') ?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
   <script src="<?= base_url('assets/') ?>bower_components/fastclick/lib/fastclick.js"></script>
   <script src="<?= base_url('assets/') ?>dist/js/adminlte.min.js"></script>
-  <script src="<?= base_url('assets/') ?>dist/js/pages/dashboard.js"></script>
   <script src="<?= base_url('assets/') ?>dist/js/demo.js"></script>
   <script src="<?= base_url('assets/') ?>plugins/iCheck/icheck.min.js"></script>
   <script type="text/javascript">
